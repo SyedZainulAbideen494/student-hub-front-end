3ad1889203a9ecf5b0dea1a6201076958119c58e
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-07-21 17:18:00 +0530

    m

--- src/quiz/resultsModal.js
+++ src/quiz/resultsModal.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './resultsModal.css'; // Add your modal-specific CSS here
 
 const ResultsModal = ({ results, onClose }) => {
+    
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = (`0${date.getMonth() + 1}`).slice(-2);
+        const day = (`0${date.getDate()}`).slice(-2);
+        return `${year}-${month}-${day}`;
+    };
+
+
     return (
         <div className="results-modal-overlay">
             <div className="results-modal">
@@ -11,7 +21,7 @@ const ResultsModal = ({ results, onClose }) => {
                         results.map(result => (
                             <li key={result.quizId} className="result-item">
                                 <span className="result-quiz-title">{result.quizTitle}</span><br/>
-                                <span className="result-date">Taken On: {result.takenAt}</span><br/>
+                                <span className="result-date">Taken On: {formatDate(result.takenAt)}</span><br/>
                                 <span className="result-score">Score: {result.score}</span>
                             </li>
                         ))
