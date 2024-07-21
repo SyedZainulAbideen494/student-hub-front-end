4e047a54c75bbe282f26dd64ab29e8aa822e79f0
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-07-21 16:43:31 +0530

    m

--- src/quiz/submitPage.js
+++ src/quiz/submitPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Confetti from 'react-confetti';
 import './quiz.css'
+import FooterNav from '../app_modules/footernav';
 
 const SubmitPage = () => {
     const location = useLocation();
@@ -13,6 +14,7 @@ const SubmitPage = () => {
         <h1 className="congratulations-title">Congratulations!</h1>
         <p className="score-text">You scored {score}%.</p>
         <p className="feedback-text">{score > 70 ? 'Awesome job!' : 'Keep practicing!'}</p>
+        <FooterNav/>
     </div>
     );
 };
