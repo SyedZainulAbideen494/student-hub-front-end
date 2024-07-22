cadc0b81529e23179ac855d03540b5c5f1591ba1
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-07-22 23:12:48 +0530

    m

--- src/app_modules/footernav.js
+++ src/app_modules/footernav.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FaUsers, FaFlask, FaStickyNote, FaCalendarAlt, FaBars, FaSignOutAlt, FaClock } from 'react-icons/fa';
 import { HiBookOpen } from 'react-icons/hi';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import './footer-nav.css';
 import axios from 'axios';
 import { API_ROUTES } from './apiRoutes';
@@ -9,6 +9,7 @@ import { API_ROUTES } from './apiRoutes';
 const FooterNav = () => {
     const [isPopupVisible, setPopupVisible] = useState(false);
     const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -23,8 +24,6 @@ const FooterNav = () => {
                     console.error('Token validation error:', error);
                     navigate('/login');
                 });
-        } else {
-            console.log('valid session')
         }
     }, [navigate]);
 
@@ -39,20 +38,20 @@ const FooterNav = () => {
 
     return (
         <div className="footer-nav">
-            <Link to='/planner' style={{ textDecoration: 'none' }}>
-                <button className="nav-btn-footer-nav active-footer-nav">
+            <Link to='/planner' style={{textDecoration: 'none'}}>
+                <button className={`nav-btn-footer-nav ${location.pathname === '/planner' ? 'active' : ''}`}>
                     <HiBookOpen className="icon-footer-nav" />
-                    <span className="btn-label">Study Planner</span>
+                    <span className="btn-label">Planner</span>
                 </button>
             </Link>
-            <Link to='/groups' style={{ textDecoration: 'none' }}>
-                <button className="nav-btn-footer-nav">
+            <Link to='/groups' style={{textDecoration: 'none'}}>
+                <button className={`nav-btn-footer-nav ${location.pathname === '/groups' ? 'active' : ''}`}>
                     <FaUsers className="icon-footer-nav" />
                     <span className="btn-label">Groups</span>
                 </button>
             </Link>
-            <Link to='/notes' style={{ textDecoration: 'none' }}>
-                <button className="nav-btn-footer-nav">
+            <Link to='/notes' style={{textDecoration: 'none'}}>
+                <button className={`nav-btn-footer-nav ${location.pathname === '/notes' ? 'active' : ''}`}>
                     <FaStickyNote className="icon-footer-nav" />
                     <span className="btn-label">Notes</span>
                 </button>
@@ -62,37 +61,37 @@ const FooterNav = () => {
                 <span className="btn-label">More</span>
             </button>
             <div className={`popup-menu-footer-nav ${isPopupVisible ? 'show-footer-nav' : ''}`}>
-                <Link to='/planner' style={{ textDecoration: 'none' }}>
+                <Link to='/planner' style={{textDecoration: 'none'}}>
                     <button className="nav-btn-footer-nav">
                         <HiBookOpen className="icon-footer-nav" />
-                        <span className="btn-label">Study Planner</span>
+                        <span className="btn-label">Planner</span>
                     </button>
                 </Link>
-                <Link to='/groups' style={{ textDecoration: 'none' }}>
+                <Link to='/groups' style={{textDecoration: 'none'}}>
                     <button className="nav-btn-footer-nav">
                         <FaUsers className="icon-footer-nav" />
                         <span className="btn-label">Groups</span>
                     </button>
                 </Link>
-                <Link to='/notes' style={{ textDecoration: 'none' }}>
+                <Link to='/notes' style={{textDecoration: 'none'}}>
                     <button className="nav-btn-footer-nav">
                         <FaStickyNote className="icon-footer-nav" />
                         <span className="btn-label">Notes</span>
                     </button>
                 </Link>
-                <Link to='/quiz/home' style={{ textDecoration: 'none' }}>
+                <Link to='/quiz/home' style={{textDecoration: 'none'}}>
                     <button className="nav-btn-footer-nav">
                         <FaFlask className="icon-footer-nav" />
                         <span className="btn-label">Quizzes</span>
                     </button>
                 </Link>
-                <Link to='/calendar' style={{ textDecoration: 'none' }}>
+                <Link to='/calendar' style={{textDecoration: 'none'}}>
                     <button className="nav-btn-footer-nav">
                         <FaCalendarAlt className="icon-footer-nav" />
                         <span className="btn-label">Calendar</span>
                     </button>
                 </Link>
-                <Link to='/pomodoro' style={{ textDecoration: 'none' }}>
+                <Link to='/pomodoro' style={{textDecoration: 'none'}}>
                     <button className="nav-btn-footer-nav">
                         <FaClock className="icon-footer-nav" />
                         <span className="btn-label">Pomodoro</span>
@@ -102,7 +101,7 @@ const FooterNav = () => {
                     <FaSignOutAlt className="icon-footer-nav" />
                     <span className="btn-label">Logout</span>
                 </button>
-                <button className="close-btn-footer-nav" onClick={togglePopup}>Close</button>
+                <button className="close-btn-footer-nav" onClick={togglePopup}>×</button>
             </div>
         </div>
     );
