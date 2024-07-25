5dff8f5c32aaa9c2b6e691252accd2d8f58fb678
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-07-25 19:19:18 +0530

    m

--- src/welcome/welcomePage.js
+++ src/welcome/welcomePage.js
@@ -1,11 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './welcome.css';
+import axios from 'axios';
+import { subscribeUser } from '../api/api'; // Correctly import subscribeUser
+import { urlBase64ToUint8Array } from '../utils/webPushUtils';
 
 const Welcome = () => {
     const [currentStep, setCurrentStep] = useState(1);
     const [transition, setTransition] = useState('');
-
+        const [subscription, setSubscription] = useState(null);
+    
+        useEffect(() => {
+          const registerServiceWorker = async () => {
+            if ('serviceWorker' in navigator) {
+              try {
+                const swRegistration = await navigator.serviceWorker.register('/service-worker.js');
+                console.log('Service Worker Registered:', swRegistration);
+      
+                const permission = await Notification.requestPermission();
+                if (permission === 'granted') {
+                  const subscription = await swRegistration.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: urlBase64ToUint8Array('BB0t-WTOpYNRM6b24mcvZKliaHnYK0umXovnqouKrFpSD8Zeq07V9N_z1jTwhenXBJ-Rlf_UxplpYculchlM3ug'),
+                  });
+                  console.log('Push Subscription:', subscription);
+                  setSubscription(subscription);
+                  await subscribeUser(subscription);
+                } else {
+                  console.warn('Notification permission denied');
+                }
+              } catch (error) {
+                console.error('Error registering service worker or subscribing:', error);
+              }
+            }
+          };
+      
+          registerServiceWorker();
+        }, []);
     const handleNext = () => {
         if (currentStep < 3) {
             setTransition('hidden');
