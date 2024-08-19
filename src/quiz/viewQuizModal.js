887395fa48a7793a870a8438d6f9203bbd61c375
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-08-19 14:19:06 +0530

    m

--- src/quiz/viewQuizModal.js
+++ src/quiz/viewQuizModal.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './viewQuizModal.css'; // Make sure to create the necessary CSS
 
 const ViewQuizModal = ({ quizResults, onClose }) => {
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = (`0${date.getMonth() + 1}`).slice(-2);
+        const day = (`0${date.getDate()}`).slice(-2);
+        const hours = (`0${date.getHours()}`).slice(-2);
+        const minutes = (`0${date.getMinutes()}`).slice(-2);
+        const seconds = (`0${date.getSeconds()}`).slice(-2);
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    };
     return (
         <div className="results-modal-overlay">
             <div className="results-modal">
@@ -11,7 +21,7 @@ const ViewQuizModal = ({ quizResults, onClose }) => {
                     {quizResults.map((result, index) => (
                         <li key={index} className="result-item">
                             <span className="result-quiz-title">{result.user_name}</span><br/>
-                            <span className="result-date">Completed At: {result.completed_at}</span><br/>
+                            <span className="result-date">Completed At: {formatDate(result.completed_at)}</span><br/>
                             <span className="result-score">Score: {result.score}</span>
                         </li>
                     ))}
