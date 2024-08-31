4fd3b3a8f65f714284f369a89bd18a712a6272fc
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-08-31 20:30:58 +0530

    m

--- src/app_modules/footernav.js
+++ src/app_modules/footernav.js
@@ -59,25 +59,42 @@ const FooterNav = () => {
                     <span className="btn-label">Notes</span>
                 </button>
             </Link>
-            <Link to='/math/solver' style={{ textDecoration: 'none' }}>
-                    <button className={`nav-btn-footer-nav ${location.pathname === '/math/solver' ? 'active' : ''}`}>
-                        <FaCalculator className="icon-footer-nav" />
-                        <span className="btn-label">Math Solver</span>
-                    </button>
-                </Link>
             <button className={`nav-btn-footer-nav ${isPopupVisible ? 'active' : ''}`} onClick={togglePopup}>
                 <FaBars className="icon-footer-nav" />
                 <span className="btn-label">More</span>
             </button>
             <div className={`popup-menu-footer-nav ${isPopupVisible ? 'show-footer-nav' : ''}`}>
                 {/* Popup Buttons */}
+                <Link to='/planner' style={{ textDecoration: 'none' }}>
+                    <button className={`nav-btn-footer-nav ${location.pathname === '/planner' ? 'active' : ''}`}>
+                        <HiBookOpen className="icon-footer-nav" />
+                        <span className="btn-label">Planner</span>
+                    </button>
+                </Link>
+                <Link to='/groups' style={{ textDecoration: 'none' }}>
+                    <button className={`nav-btn-footer-nav ${location.pathname === '/groups' ? 'active' : ''}`}>
+                        <FaUsers className="icon-footer-nav" />
+                        <span className="btn-label">Groups</span>
+                    </button>
+                </Link>
+                <Link to='/notes' style={{ textDecoration: 'none' }}>
+                    <button className={`nav-btn-footer-nav ${location.pathname === '/notes' ? 'active' : ''}`}>
+                        <FaStickyNote className="icon-footer-nav" />
+                        <span className="btn-label">Notes</span>
+                    </button>
+                </Link>
                 <Link to='/search' style={{ textDecoration: 'none' }}>
                     <button className={`nav-btn-footer-nav ${location.pathname === '/search' ? 'active' : ''}`}>
                         <FaSearch className="icon-footer-nav" />
                         <span className="btn-label">Search</span>
                     </button>
                 </Link>
-               
+                <Link to='/math/solver' style={{ textDecoration: 'none' }}>
+                    <button className={`nav-btn-footer-nav ${location.pathname === '/math/solver' ? 'active' : ''}`}>
+                        <FaCalculator className="icon-footer-nav" />
+                        <span className="btn-label">Math Solver</span>
+                    </button>
+                </Link>
                 <Link to='/science/helper' style={{ textDecoration: 'none' }}>
                     <button className={`nav-btn-footer-nav ${location.pathname === '/science/helper' ? 'active' : ''}`}>
                         <GiMaterialsScience className="icon-footer-nav" />
