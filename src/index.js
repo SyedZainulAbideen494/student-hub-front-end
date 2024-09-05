843095fac693ce154fafe0ed9af6a59abfb06ea1
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-09-05 19:59:04 +0530

    m

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
 import App from './App';
 import 'animate.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 // Create a root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,3 +13,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
