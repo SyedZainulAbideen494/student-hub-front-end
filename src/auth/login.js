b3449bccbba9c175c6dd0725e745eb271539d6d8
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-09-06 21:19:31 +0530

    m

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaPhone, FaLock, FaUser, FaEnvelope } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import './login.css';
