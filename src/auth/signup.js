aea3d6c7853522e82cc541d77141f49dd2d1afa0
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2024-10-05 11:58:09 +0530

    Initial commit

--- src/auth/signup.js
+++ src/auth/signup.js
@@ -152,7 +152,7 @@ const SignUp = () => {
     };
 
     // Check token and redirect
-  {/*  const checkTokenAndRedirect = async (token, navigate) => {
+    const checkTokenAndRedirect = async (token, navigate) => {
     try {
       const response = await axios.post(API_ROUTES.sessionCheck, { token });
   
@@ -169,7 +169,7 @@ const SignUp = () => {
   useEffect(() => {
     const token = localStorage.getItem('token'); // Replace with actual token retrieval logic
     checkTokenAndRedirect(token, nav);
-  }, [nav]); */}
+  }, [nav]); 
 
 
     const handleSuggestionClick = (suggestion) => {
