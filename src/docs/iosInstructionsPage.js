084d90f4ebdf12a43c4e7437e2dfd6dabcb64ffd
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2024-10-12 17:59:12 +0530

    m

--- src/docs/iosInstructionsPage.js
+++ src/docs/iosInstructionsPage.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import './IOSInstructions.css'; // Create this CSS file for styling
+import './IOSInstructions.css'; // Ensure to create this CSS file for dark theme styling
 
 const IOSInstructions = () => {
   return (
-    <div className="instructions-container__ios__instructions">
-      <h1 className="title__ios__instructions">We're Working on Our iOS App!</h1>
-      <p className="intro__ios__instructions">While we’re perfecting the iOS app, you can still add Edusify to your home screen as a shortcut for quick access. Follow the steps below:</p>
+    <div className="instructions-container__ios__instructions dark-theme__ios__instructions">
+      <h1 className="title__ios__instructions">We’re Extremely Sorry!</h1>
+      <p className="intro__ios__instructions">While we’re working hard to perfect the iOS app, you can still add Edusify to your home screen as a shortcut for quick access. Follow the steps below:</p>
       
       <div className="steps__ios__instructions">
         <div className="step__ios__instructions">
           <h2 className="step-title__ios__instructions">Step 1</h2>
-          <p className="step-description__ios__instructions">Open Safari and navigate to our <a href='https://edusify.vercel.app' target="_blank" rel="noopener noreferrer">web app</a>.</p>
+          <p className="step-description__ios__instructions">Open Safari and navigate to our <a href='https://edusify.vercel.app/ios/instructions/download' target="_blank" rel="noopener noreferrer">web app</a>.</p>
         </div>
         <div className="step__ios__instructions">
           <h2 className="step-title__ios__instructions">Step 2</h2>
