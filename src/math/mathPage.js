f070bdaf5358de8451015c77bf84cc4b93ce0bdb
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2024-12-15 23:50:51 +0530

    m

--- src/math/mathPage.js
+++ src/math/mathPage.js
@@ -639,13 +639,30 @@ useEffect(() => {
       className="modal-input__magic__modal__ai__page"
     />
     {/* Removed the textarea, using selected content instead */}
+<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
     <button
-      onClick={() => handleSubmitFlashcards(magicModalContent)} // Send selected content directly
-      disabled={isGeneratingFlashcards}
-      className="modal-btn__magic__modal__ai__page"
+  className="flashcard__set__page__modal-generate btn__set__page__buttons"
+  onClick={() => handleSubmitFlashcards(magicModalContent)} 
+  disabled={isGeneratingFlashcards}
+>
+  <div className={`sparkle__set__page__buttons ${isGeneratingFlashcards ? 'animating' : ''}`}>
+    <svg
+      height="24"
+      width="24"
+      fill="#FFFFFF"
+      viewBox="0 0 24 24"
+      data-name="Layer 1"
+      id="Layer_1"
+      className="sparkle__set__page__buttons"
     >
+      <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
+    </svg>
+    <span className="text__set__page__buttons">
       {isGeneratingFlashcards ? 'Generating...' : 'Generate'}
-    </button>
+    </span>
+  </div>
+</button>
+</div>
   </div>
 </Modal>
 
@@ -670,10 +687,32 @@ useEffect(() => {
         className="modal-input__magic__modal__ai__page"
       />
     </label>
-    {/* Removed the textarea, using selected content instead */}
-    <button type="submit" disabled={isGeneratingQuiz} className="modal-btn__magic__modal__ai__page">
-      {isGeneratingQuiz ? 'Generating...' : 'Generate Quiz'}
-    </button>
+
+     {/* Removed the textarea, using selected content instead */}
+<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+    <button
+  className="flashcard__set__page__modal-generate btn__set__page__buttons"
+   type="submit"
+  disabled={isGeneratingQuiz}
+>
+  <div className={`sparkle__set__page__buttons ${isGeneratingQuiz ? 'animating' : ''}`}>
+    <svg
+      height="24"
+      width="24"
+      fill="#FFFFFF"
+      viewBox="0 0 24 24"
+      data-name="Layer 1"
+      id="Layer_1"
+      className="sparkle__set__page__buttons"
+    >
+      <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
+    </svg>
+    <span className="text__set__page__buttons">
+      {isGeneratingQuiz ? 'Generating...' : 'Generate'}
+    </span>
+  </div>
+</button>
+</div>
   </form>
 </Modal>
 
