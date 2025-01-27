49d9c824a2e9733a7cf7589b3ea6398ba5469d01
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2025-01-27 08:43:51 +0530

    m

--- src/app_modules/footernav.js
+++ src/app_modules/footernav.js
@@ -108,7 +108,7 @@ const FooterNav = () => {
                 <button className={`nav-btn-footer-nav ${location.pathname === '/notes/view' ? 'active' : ''}`}>
                     <FaBook className="icon-footer-nav" />
                     <span className="btn-label">Notes</span>
-
+                    <span className="new-label-footer-nav">New fetaures</span>
                 </button>
             </Link>
             <Link to='/flashcard' style={{ textDecoration: 'none' }}>
