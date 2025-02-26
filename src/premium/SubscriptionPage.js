25c574f95061a519eca15d50fbe68e2797e38d3c
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2025-02-26 20:44:25 +0530

    m

--- src/premium/SubscriptionPage.js
+++ src/premium/SubscriptionPage.js
@@ -269,22 +269,22 @@ useEffect(() => {
         active={duration === 'weekly'}
         onClick={() => {
           setDuration('weekly');
-          setAmount(79);
+          setAmount(59);
         }}
       >
         <span>Weekly Access</span>
-        <span>₹79/week</span>
+        <span>₹59/week</span>
       </Plan>
   
       <Plan
         active={duration === 'monthly'}
         onClick={() => {
           setDuration('monthly');
-          setAmount(229);
+          setAmount(119);
         }}
       >
         <span>Monthly Plan</span>
-        <span>₹229/mo</span>
+        <span>₹119/mo</span>
       </Plan>
   
     
