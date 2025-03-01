8ef6cb411d147ee3a185a0bde0ee29a64cad18aa
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2025-03-01 11:20:43 +0530

    m

--- src/home/homeMain.js
+++ src/home/homeMain.js
@@ -139,7 +139,7 @@ const HomeMain = () => {
   return (
     <Fragment>
         <>
-        <SubscriptionRedirector/>
+
           <ReviewModal />
           <HomeTopBoxes />
           <FeedbackFormWeekly />
