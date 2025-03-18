3dec17a5cd42a9fdda9e47ac842e240d185a5712
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2025-03-18 23:45:38 +0530

    m

--- src/home/homeMain.js
+++ src/home/homeMain.js
@@ -147,7 +147,6 @@ const HomeMain = () => {
           <ReviewModal />
           <HomeTopBoxes />
           <PomodoroChallengeBannerPremium/>
-          <GuideBanner/>
           <FeedbackFormWeekly />
           <TodayEventsAndTasks />
           <FriendsDashboard />
