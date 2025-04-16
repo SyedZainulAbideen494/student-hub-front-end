f4c48501edf178d538a4e86800d6d820c5355f53
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2025-04-16 20:31:07 +0530

    m

--- src/quiz/QuizHomePage.js
+++ src/quiz/QuizHomePage.js
@@ -154,11 +154,7 @@ const handleShareClick = (quiz) => {
             <div className="quiz-header-container-quiz-page">
 <div className='search-bar-container'>
                 <form className="form__search__bar__quiz__page">
-      <button>
-          <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="search">
-              <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" stroke-width="1.333" stroke-linecap="round" stroke-linejoin="round"></path>
-          </svg>
-      </button>
+
       <input 
                      className="input__search__bar__quiz__page"  
                      type="text"
