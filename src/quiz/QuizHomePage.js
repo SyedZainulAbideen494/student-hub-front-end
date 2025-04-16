ca6ef533392348f9fc68eea6c2799b2b842fade5
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2025-04-16 20:59:37 +0530

    m

--- src/quiz/QuizHomePage.js
+++ src/quiz/QuizHomePage.js
@@ -171,20 +171,7 @@ const handleShareClick = (quiz) => {
   </div>
 
                 <div className="quiz-header-buttons-quiz-page">
-    <button className='quiz-create-btn-home-page' onClick={handleCreateQuiz}>
-        <span className='quiz-create-btn-home-page-span'>
-            <svg
-                height="20"
-                width="20"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z" fill="currentColor"></path>
-            </svg>
-            Create
-        </span>
-    </button>
+
 
     <button className='quiz-create-btn-home-page' onClick={handleResultsClick}>
         <span className='quiz-create-btn-home-page-span'>
